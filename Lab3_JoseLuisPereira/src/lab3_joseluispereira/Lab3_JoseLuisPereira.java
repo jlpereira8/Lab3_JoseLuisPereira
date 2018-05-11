482e64dd1846e02ca7f1efe349e05ad9c802fe30
jlpereira8 <jlpereira355@gmail.com>
2018-05-11 13:44:43 -0600

    Clase personajes

---
+++ Lab3_JoseLuisPereira/src/lab3_joseluispereira/Lab3_JoseLuisPereira.java
@@ -0,0 +1,33 @@
+
+package lab3_joseluispereira;
+
+import java.util.Scanner;
+
+
+public class Lab3_JoseLuisPereira {
+
+    static Scanner lea=new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        int opw=0,ops=0;
+        while(opw!=3){
+            System.out.println("*******************Menu*******************");
+            System.out.println("1) Iniciar");
+            System.out.println("2) Nivel de dificultad ");
+            System.out.println("3) Salir");
+            ops=lea.nextInt();
+            switch (ops){
+                case 1:
+                    
+                break;
+                case 2:
+                    
+                break;
+                case 3:
+                    opw=3;
+                break;
+            }
+        }
+    }
+    
+}
